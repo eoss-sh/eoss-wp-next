fd8bdc7bafa344fa4b43f3c324e1b95654cb9fe4
eoss-sh <sh@eoss.ch>
2023-10-12 22:35:27 +0200

    fonts

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,16 +1,31 @@
+import { Hanken_Grotesk, JetBrains_Mono } from 'next/font/google'
 import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
 
+const jb = JetBrains_Mono({
+    subsets: ['latin'],
+    weight: '100',
+    display: 'swap',
+    variable: '--font-jb',
+})
+const hanken = Hanken_Grotesk({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-hanken',
+})
+
 export default function Layout({ preview, children }) {
-  return (
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Meta />
+            <div
+                className={`${jb.variable} ${hanken.variable} min-h-screen font-sans`}
+            >
+                <Alert preview={preview} />
+                <main>{children}</main>
+            </div>
+            <Footer />
+        </>
+    )
 }
